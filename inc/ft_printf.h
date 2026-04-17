3200b2072ccf0931da3ef8ed4fb4447bc718fdf4
klongrich <longrichk@gmail.com>
2026-04-16 22:52:20 -0400

    adding support for %p to print the address of the pointer

--- inc/ft_printf.h
+++ inc/ft_printf.h
@@ -1,3 +1,3 @@
 #include "../libft/libft.h"
 
-void    ft_printf(char *str, ...); 
+int    ft_printf(char *str, ...); 
--- src/ft_printf.c
+++ src/ft_printf.c
@@ -1,16 +1,56 @@
 #include "ft_printf.h"
-#include "ft_printf.h"
+
+int	putunit_max(unsigned long num, int base) {
+	char	*hex;
+	char	buffer[50];
+	int	i;
+	int	count;
+
+	hex = "0123456789abcdef";
+	i = 49;
+	count = 0;
+	buffer[i] = '\0';
+	if (num == 0)
+		buffer[--i] = '0';
+	while (num > 0) {
+		buffer[--i] = hex[num % base];
+		num /= base;
+	}
+	while (buffer[i]) {
+		write(1, &buffer[i], 1);
+		count++;
+		i++;
+	}
+	return (count);
+}
 
 
-void	ft_printf(char *str, ...) {
+int	put_pointer(void *pointer) {
+	unsigned long long	address;
+	int			count;
+
+	count = 0;
+	address = (unsigned long long) pointer;
+	if (pointer == NULL)
+		ft_putstr("(nil)");
+	ft_putstr("0x");
+	count += putunit_max(address, 16);
+	return(count);
+}
+
+
+int	ft_printf(char *str, ...) {
 	va_list list;
 	int	i;
+	int 	count;
+	
 
 	i = 0;
+	count = 0;
 	va_start(list, str);
 	while (str[i]) {
 		while (str[i] != '%' && str[i]) {
-			ft_putchar(str[i]);
+			count += ft_putchar(str[i]);
 			i++;
 		}
 		if (!str[i])
@@ -20,6 +60,9 @@ void	ft_printf(char *str, ...) {
 			ft_putnbr(va_arg(list, int));
 		else if (str[i] == 's')
 			ft_putstr(va_arg(list, char*));
+		else if (str[i] == 'p')
+			put_pointer(va_arg(list, void*));	
 		str++;
 	}
+	return(count);
 }
