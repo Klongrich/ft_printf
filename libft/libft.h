f3f623efe6050c7560845904e8fb753de930f84d
klongrich <longrichk@gmail.com>
2026-04-16 22:51:32 -0400

    updating libft function to return an integer to help count total amount of characters printed

--- libft/libft.h
+++ libft/libft.h
@@ -54,7 +54,7 @@ int					ft_strnequ(char const *s1, char const *s2, size_t n);
 char				*ft_strsub(char const *s, unsigned int start, size_t len);
 char				*ft_strjoin(char const *s1, char const *s2);
 char				*ft_strtrim(char const *s);
-void				ft_putchar(char c);
+int				ft_putchar(char c);
 void				ft_putstr(char const *s);
 void				ft_putendl(char const *s);
 void				ft_putendl_fd(char const *s, int fd);
