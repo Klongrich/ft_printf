7710c56b06736e9a0d08ffe2c117392c94e2c827
klongrich <longrichk@gmail.com>
2026-04-23 21:39:48 -0400

    fixing %hhs passed as arg

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -1159,6 +1159,8 @@ int	ft_printf(char *str, ...) {
 				put_8bit_hex(va_arg(list, unsigned int), 0, flags);
 			else if (str[i + 2] == 'X')
 				put_8bit_hex(va_arg(list, unsigned int), 1, flags);
+			else if (str[i + 2] == 's')
+				count += put_string_args(va_arg(list, char*), flags);
 			i += 2;
 		}
 		else if (str[i] == 'h') {
