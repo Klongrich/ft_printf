62aff5f8d214b00387e97aef2ab55820b44dd54a
klongrich <longrichk@gmail.com>
2026-04-30 07:14:54 -0400

    adding check for return value in printf

--- inc/ft_printf.h
+++ inc/ft_printf.h
@@ -2,11 +2,19 @@
 #include <stdio.h>
 
 // Standard C99 macro
-#define CHECK(format, ...) \
-    printf("checking %s", format); \
-    printf("ORIG: " format, __VA_ARGS__); \
-    ft_printf("MINE: " format, __VA_ARGS__); \
-    printf("\n");
+#define CHECK(format, ...) do { \
+    int r1, r2; \
+    printf("checking %s\n", format); \
+    r1 = printf("ORIG: " format, __VA_ARGS__); \
+    printf("\n"); \
+    r2 = ft_printf("MINE: " format, __VA_ARGS__); \
+    printf("\n"); \
+    if (r1 != r2) \
+        printf("\033[31m  [KO] Return values differ! (Orig: %d, Mine: %d)\033[0m\n", r1, r2); \
+    else \
+        printf("\033[32m  [OK] Return values match: %d\033[0m\n", r1); \
+    printf("--------------------\n"); \
+} while (0)
 
 int    ft_printf(char *str, ...); 
 void    put_64bit_ull(unsigned long long num);
